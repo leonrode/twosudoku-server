07f05a4844f310b5d8d7d40aabc7db6217a0f333
Leon Rode <leon.rode13@gmail.com>
2021-07-11 13:10:47 -0400

    Delete funcs.js

--- funcs.js
+++
@@ -1,8 +0,0 @@
-async function findBoard(difficulty) {
-  const url = `http://www.cs.utep.edu/cheon/ws/sudoku/new/?size=9&level=2`; // 9x9 with 2 difficulty
-  const response = await axios.get(url);
-  const data = await response.data;
-  return data;
-}
-
-module.exports.findBoard = findBoard;
